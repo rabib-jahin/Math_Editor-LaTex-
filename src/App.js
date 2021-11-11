545fdd374f27a4f716f04e35fd296d8fc3314176
rabib-jahin <rabibjahin2000@gmail.com>
2021-11-11 15:02:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Latex from "react-latex";
 
-import React,{useState} from "react";
-import Topic from "./Topic"
-import Series from "./Series"
-import Recommend  from './Recommend';
-import Recommend2  from './Recommend2';
-
-
+import "./App.css";
 
 function App() {
-const [state,setState]=useState(0);
-const handleClick=()=>{
-let c=state;
-if(c==2){
-  c=0;
+  return (<></>)
 }
-else
-c+=1
-  setState(c)
-
-console.log(state)
-}
-  return (
-    <div>
-    <div class="container">
-<div id={state==0?"rec-pointer":"rectangle"} >
-
- 
 
 
-
-</div>
-{
-  state===1?<><Topic text={"Logical Reasoning"}/><Recommend2 text={"Recommend"}/></>:state==2?<><Topic text={"Logical Reasoning"}/><Series text={"Rearrange"}/><Recommend text={"Recommend"}/></>:null
-}
-</div>
-<button style={{marginLeft:"1000px"}} onClick={handleClick}>Click</button>
-</div>
-  );
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Latex from "react-latex";
+
+import "./App.css";
+
+function App() {
+  const [expression, setExpression] = useState(" ");
+ 
+  return (
+    <div className="App">
+      <h1>Math Editor and Viewer Example</h1>
+      <MathInput onChange={ev => setExpression(ev.target.latex())} />
+      <div style={{ margin: 20 }} />
+      <Latex>{`View: $${expression}$`}</Latex>
+   
+    </div>
+  );
+}
+
+const MathInput = ({ onChange, ...rest }) => {
+  const ref = useRef();
+  useEffect(() => {
+    window.Guppy.use_osk(new window.GuppyOSK({ goto_tab: "arithmetic", attach: "focus" }));
+    const guppy = new window.Guppy(ref.current);
+    guppy.event("change", onChange);
+  }, []);
+
+  return <div ref={ref} {...rest} />;
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
